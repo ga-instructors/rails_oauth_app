33134f7e67c1dab57b13260fc8cfacf3dba7abf0
h4w5 <philip@h4w5.com>
2015-05-12 14:58:00 -0400

    implement hand-rolled oauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :users
   
   resource :session, only: [:new, :create, :destroy]
-  get "/logout", to: "sessions#destroy"
+  get "/logout",         to: "sessions#destroy"
+  get "/oauth_callback", to: "sessions#create"
   
   root "sessions#new"
 end
