ae9a4bae074793171866f6466c4f364b0675c8c3
h4w5 <philip@h4w5.com>
2015-05-13 11:09:17 -0400

    implement with oauth2 gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,30 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # make the create token request to the identity provider
-    token = request_token
+    # make the create token request to the identity provider, returns an
+    # OAuth2::AccessToken instance
+    access_token = github_oauth_client.get_token params[:code], {
+      redirect_uri: redirect_uri
+    }
+
     # make a second request, with the token, for the authenticated
     # user's basic information (credentials)
-    credentials = get_credentials_with token
-    # see if the user exists yet, and if not create them
+    credentials = access_token.get('https://api.github.com/user', {
+      headers: {
+        'Authorization' => "token #{access_token.token}",
+        'User-Agent'    => 'request'
+      }
+    }).parsed
+
+    # see if the user exists yet (if not, create them) and
     # then log them in
-    user = log_in_user_by credentials
+    user = log_in_user_with({
+      access_token: access_token.token,
+      oauth_uid:    credentials['id'],
+      name:         credentials['name'],
+      email:        credentials['email']
+    })
+
     # and then redirect to that user's home page
     redirect_to user_path(user)
   end
@@ -24,64 +40,30 @@ class SessionsController < ApplicationController
   helper_method :code_uri
 
   private
-
-  GITHUB_CODE_URI  = 'https://github.com/login/oauth/authorize'
-  GITHUB_TOKEN_URI = 'https://github.com/login/oauth/access_token'
-  API_PROFILE_URI  = 'https://api.github.com/user'
-  
-  # ENV["GITHUB_OAUTH_ID"]
-  # ENV["GITHUB_OAUTH_SECRET"]
   
   # make the redirect work for any port and server, instead of hard-coding it!
   def redirect_uri
-    root_url[0..-2] + oauth_callback_path
+    @redirect_uri ||= root_url[0..-2] + oauth_callback_path
+  end
+
+  def github_oauth_client
+    @github_oauth_client ||= OAuth2::Client.new(
+      ENV["GITHUB_OAUTH_ID"], 
+      ENV["GITHUB_OAUTH_SECRET"], 
+      site:          'https://github.com',
+      authorize_url: '/login/oauth/authorize',
+      token_url:     '/login/oauth/access_token'
+    ).auth_code
   end
 
   def code_uri
-    query_params = "?" + URI.encode_www_form({
-      response_type: 'code',
-      client_id:     ENV["GITHUB_OAUTH_ID"],
-      redirect_uri:  redirect_uri,
-      scope:         '' # the default works for GitHub
-    })
-    GITHUB_CODE_URI + query_params
+    @code_uri ||= github_oauth_client.authorize_url(
+      :redirect_uri => redirect_uri,
+      :scope => ''
+    )
   end
 
-  def request_token
-    response = HTTParty.post GITHUB_TOKEN_URI, {
-      body: {
-        :code          => params[:code],
-        :client_id     => ENV["GITHUB_OAUTH_ID"],
-        :client_secret => ENV["GITHUB_OAUTH_SECRET"],
-        :redirect_uri  => redirect_uri,
-        :grant_type    => "authorization_code"
-      },
-      headers: {
-        'Accept' => 'application/json'
-      },
-      format: :json # parse the response as JSON
-    }
-    response['access_token']
-  end
-
-  def get_credentials_with(token)
-    response = HTTParty.get API_PROFILE_URI, {
-      headers: {
-        'Authorization' => "token #{token}",
-        'User-Agent'    => 'request'
-      },
-      format: :json # parse the response as JSON
-    }
-
-    return {
-      access_token: token,
-      oauth_uid:    response['id'],
-      name:         response['name'],
-      email:        response['email']
-    }
-  end
-
-  def log_in_user_by(credentials)
+  def log_in_user_with(credentials)
     # find if any user has that oauth_uid
     user = User.find_or_initialize_by(oauth_uid: credentials[:oauth_uid])
     
