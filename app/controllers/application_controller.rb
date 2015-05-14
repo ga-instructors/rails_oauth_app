635b5f1242dcb7edb24544306b78d929ddab5c07
h4w5 <philip@h4w5.com>
2015-05-14 18:36:28 -0400

    implement with github_api gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,18 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def current_user_client
+    @current_user_client ||= Github.new(oauth_token: session[:access_token])
+  end
+
+  def current_user_api_data
+    @current_user_api_data ||= current_user_client.get_request("user").to_hash
+  end
+
   def logged_in?
     current_user.present?
   end
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :current_user_client, 
+                :current_user_api_data, :logged_in?
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,26 +4,61 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # find if any user has that email
-    user = User.find_by(email: params[:email])
+    # use the Github instance representing the application to get an access
+    # token for the user represented by their auth code
+    access_token = application_client.get_token(params[:code]).token
     
-    # if that user exists and it has a password that was sent
-    if user && user.authenticate(params[:password])
-      
-      # save the user_id in the session hash
-      session[:user_id] = user.id
+    # store the access token in the current session
+    session[:access_token] = access_token
 
-      # and redirect to that user's home page
-      redirect_to users_path
-    else
-      # the email/password is wrong!
-      @message = "This email and password combination does not exist."
-      render :new
-    end
+    # use the Github instance representing the authenticated user to grab their
+    # profile information
+    user = log_in_user_with({
+      oauth_uid:    current_user_api_data['id'],
+      name:         current_user_api_data['name'],
+      email:        current_user_api_data['email']
+    })
+
+    # and then redirect to that user's home page
+    redirect_to user_path(user)
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  helper_method :application_client
+
+  private
+  
+  # make the redirect work for any port and server, instead of hard-coding it!
+  def redirect_uri
+    @redirect_uri ||= root_url[0..-2] + oauth_callback_path
+  end
+
+  def application_client
+    @application_client ||= Github.new(
+      client_id:     ENV["GITHUB_OAUTH_ID"],
+      client_secret: ENV["GITHUB_OAUTH_SECRET"]
+    )
+  end
+
+  def log_in_user_with(credentials)
+    # find if any user has that oauth_uid
+    user = User.find_or_initialize_by(oauth_uid: credentials[:oauth_uid])
+    
+    # if none does, add them to the database
+    if user.new_record?
+      user.name  = credentials[:name]
+      user.email = credentials[:email]
+      user.save
+    end
+
+    # save the user_id and access token in the session hash
+    session[:user_id] = user.id
+
+    # return the user model
+    user
   end
 end
