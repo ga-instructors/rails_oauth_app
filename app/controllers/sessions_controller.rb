ad47e9af50f94a18c8e536b9e7a12da5d87079b4
h4w5 <philip@h4w5.com>
2015-05-14 18:30:34 -0400

    implement with oauth2 gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,26 +4,81 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # find if any user has that email
-    user = User.find_by(email: params[:email])
-    
-    # if that user exists and it has a password that was sent
-    if user && user.authenticate(params[:password])
-      
-      # save the user_id in the session hash
-      session[:user_id] = user.id
+    # make the create token request to the identity provider, returns an
+    # OAuth2::AccessToken instance
+    access_token = github_oauth_client.get_token params[:code], {
+      redirect_uri: redirect_uri
+    }
 
-      # and redirect to that user's home page
-      redirect_to users_path
-    else
-      # the email/password is wrong!
-      @message = "This email and password combination does not exist."
-      render :new
-    end
+    # make a second request, with the token, for the authenticated
+    # user's basic information (credentials)
+    credentials = access_token.get('https://api.github.com/user', {
+      headers: {
+        'Authorization' => "token #{access_token.token}",
+        'User-Agent'    => 'request'
+      }
+    }).parsed
+
+    # see if the user exists yet (if not, create them) and
+    # then log them in
+    user = log_in_user_with({
+      access_token: access_token.token,
+      oauth_uid:    credentials['id'],
+      name:         credentials['name'],
+      email:        credentials['email']
+    })
+
+    # and then redirect to that user's home page
+    redirect_to user_path(user)
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  helper_method :code_uri
+
+  private
+  
+  # make the redirect work for any port and server, instead of hard-coding it!
+  def redirect_uri
+    @redirect_uri ||= root_url[0..-2] + oauth_callback_path
+  end
+
+  def github_oauth_client
+    @github_oauth_client ||= OAuth2::Client.new(
+      ENV["GITHUB_OAUTH_ID"], 
+      ENV["GITHUB_OAUTH_SECRET"], 
+      site:          'https://github.com',
+      authorize_url: '/login/oauth/authorize',
+      token_url:     '/login/oauth/access_token'
+    ).auth_code
+  end
+
+  def code_uri
+    @code_uri ||= github_oauth_client.authorize_url(
+      :redirect_uri => redirect_uri,
+      :scope => ''
+    )
+  end
+
+  def log_in_user_with(credentials)
+    # find if any user has that oauth_uid
+    user = User.find_or_initialize_by(oauth_uid: credentials[:oauth_uid])
+    
+    # if none does, add them to the database
+    if user.new_record?
+      user.name  = credentials[:name]
+      user.email = credentials[:email]
+      user.save
+    end
+
+    # save the user_id and access token in the session hash
+    session[:access_token] = credentials[:token]
+    session[:user_id]      = user.id
+
+    # return the user model
+    user
   end
 end
